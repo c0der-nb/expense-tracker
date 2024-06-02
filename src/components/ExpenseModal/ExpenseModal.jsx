4db2a7a780cafd8415a35c5b1ff76a1397e5f7c5
Aditya Gupta <gaditya060@gmail.com>
2024-06-02 22:15:56 +0530

    remove input stuff from TextField

--- src/components/ExpenseModal/ExpenseModal.jsx
+++ src/components/ExpenseModal/ExpenseModal.jsx
@@ -50,13 +50,13 @@ function ExpenseModal({type, selectedExpense, addExpenseHandler, updateExpenseHa
                 <p className={styles.modalHeading}>Add Expenses</p>
                 <form onSubmit={addHandler} className={styles.expenseFormContainer}>
                     <TextField onChange={(e) => setTitle(e.target.value)} id="title" label="Title" variant="outlined" />
-                    <TextField sx={{"& fieldset": { border: 'none' },}} onChange={((e) => setPrice(e.target.value))} className={styles.inputBox} type="text" label="Price" id="price" variant="outlined" required />
-                    <TextField sx={{"& fieldset": { border: 'none' },}} select onChange={((e) => setCategory(e.target.value))} className={styles.inputBox} name="Category Dropdown" label="Select Category" id="category" required>
+                    <TextField onChange={((e) => setPrice(e.target.value))} type="text" label="Price" id="price" variant="outlined" />
+                    <TextField select onChange={((e) => setCategory(e.target.value))} name="Category Dropdown" label="Select Category" id="category">
                         <MenuItem value="Entertainment">Entertainment</MenuItem>
                         <MenuItem value="Food">Food</MenuItem>
                         <MenuItem value="Travel">Travel</MenuItem>
                     </TextField>
-                    <TextField sx={{"& fieldset": { border: 'none' },}} onChange={(e) => setDate(e.target.value)} className={styles.inputBox} type="date" placeholder="dd/mm/yyyy" id="date" required />
+                    <TextField onChange={(e) => setDate(e.target.value)} type="date" placeholder="dd/mm/yyyy" id="date" />
                     <button type="submit" className={`${styles.addButton} cursor-pointer`}>Add Expense</button>
                     <button className={`${styles.cancel} cursor-pointer`} onClick={cancelHandler}>Cancel</button>
                 </form>
@@ -71,14 +71,14 @@ function ExpenseModal({type, selectedExpense, addExpenseHandler, updateExpenseHa
                 >
                 <p className={styles.modalHeading}>Edit Expenses</p>
                 <form onSubmit={updateHandler} className={styles.expenseFormContainer}>
-                    <TextField sx={{"& fieldset": { border: 'none' },}} onChange={(e) => setTitle(e.target.value)} className={styles.inputBox} type="text" label="Title" variant="outlined" defaultValue={selectedExpense.title} id="title" required />
-                    <TextField sx={{"& fieldset": { border: 'none' },}} onChange={(e) => setPrice(e.target.value)} className={styles.inputBox} type="text" label="Price" variant="outlined" defaultValue={selectedExpense.price} id="price" required />
-                    <TextField sx={{"& fieldset": { border: 'none' },}} select onChange={(e) => setCategory(e.target.value)} className={styles.inputBox} name="Category Dropdown" label="Select Category" variant="outlined" defaultValue={selectedExpense.category} id="category" required>
+                    <TextField onChange={(e) => setTitle(e.target.value)} type="text" label="Title" variant="outlined" defaultValue={selectedExpense.title} id="title" />
+                    <TextField onChange={(e) => setPrice(e.target.value)} type="text" label="Price" variant="outlined" defaultValue={selectedExpense.price} id="price" />
+                    <TextField select onChange={(e) => setCategory(e.target.value)} name="Category Dropdown" label="Select Category" variant="outlined" defaultValue={selectedExpense.category} id="category">
                         <MenuItem value="Entertainment">Entertainment</MenuItem>
                         <MenuItem value="Food">Food</MenuItem>
                         <MenuItem value="Travel">Travel</MenuItem>
                     </TextField>
-                    <TextField sx={{"& fieldset": { border: 'none' },}} onChange={(e) => setDate(e.target.value)} className={styles.inputBox} type="date" label="dd/mm/yyyy" variant="outlined" defaultValue={selectedExpense.date} id="date" required/>
+                    <TextField onChange={(e) => setDate(e.target.value)} type="date" label="dd/mm/yyyy" variant="outlined" defaultValue={selectedExpense.date} id="date"/>
                     <button type="submit" className={`${styles.addButton} cursor-pointer`}>Update Expense</button>
                     <button className={`${styles.cancel} cursor-pointer`} onClick={cancelHandler}>Cancel</button>
                 </form>
